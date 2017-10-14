300ed5993ec8289c5b6dc17435ba017e46323562
SirawichDev <sirawit0676@gmail.com>
2017-10-14 20:14:16 +0700

    update config blockchain and Testing file

---
+++ TestingBlock.py
@@ -0,0 +1,20 @@
+import block
+import Chain
+
+chain = Chain.Blockchain()
+
+first = block.Block('first')
+second = block.Block('second')
+third = block.Block('third')
+
+chain.add_block(first)
+chain.add_block(second)
+chain.add_block(third)
+
+first.update_data('so broke')
+
+print(chain.broken)  # True
+
+chain.repair()
+
+print(chain.broken)  # False
